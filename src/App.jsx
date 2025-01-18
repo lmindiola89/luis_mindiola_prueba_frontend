7d9751cb9c0d63a2f3e016505b4d8b6dad616d04
lmindiola89 <lmindiola89@gmail.com>
2025-01-17 22:54:12 -0500

    making changes to upload netlify

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import "./app.scss";
+import "./index.scss";
 import Cardslider from "./components/Cardslider/Cardslider";
 import Forms from "./components/Forms/Forms";
 import Footer from "./components/Footer/Footer";
